22caba5c86747f0c4c5081f0d775efcdf634724a
100hoursDEV <98231532+100hoursDEV@users.noreply.github.com>
2022-02-18 07:43:22 -0800

    Ch5. Finished:  Jinja2 autocomplete in PyCharm

---
+++ first_site/README.md
@@ -0,0 +1,11 @@
+# Work in Progress
+
+## command line history
+
+```
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+```
+
+
--- first_site/app.py
+++ first_site/app.py
@@ -1,8 +1,26 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+
+# simulate database:
+def get_latest_packages():
+    return [
+        {"name": "flask", "version": "1.2.3", "description": "jsdfhgdhjfgsdjhfgsdjfgsdjhfgsdhjfgsjfgsj"},
+        {"name": "pip", "version": "1.3", "description": "jsdfhgddhjfgsjfgsj"},
+        {"name": "reportlab", "version": "4.3", "description": "jsdfhgdhjfgsdjhfgsdjfgsdjhfgsdhjfgsjfgsj"},
+        {"name": "flask", "version": "8.32.3", "description": "jsdfhgdhjfgsdjhfgsdjfgsdjhfgsdhjfgsjfgsj"},
+        {"name": "Jinja2", "version": "1.3.33", "description": "jsdfhgdhjfgsdjhfgsdjfgsdjhfgsdhjfgsjfgsj"},
+        {"name": "setuptools", "version": "1.10.93", "description": "jsdfhgdhjfgsdjhfgsdjfgsdjhfgsdhjfgsjfgsj"},
+    ]
+
 
 
 @app.route("/")
 def index():
-    return "hello world"
+    test_packages = get_latest_packages()
+    return render_template("index.html", packages=test_packages)
+    # seems like the templates folder name is the default for flask so it does not need an explicit path
+
+if __name__ == "__main__":
+    app.run(debug=True)
