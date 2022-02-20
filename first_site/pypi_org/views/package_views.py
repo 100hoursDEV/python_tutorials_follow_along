f47b635b90f04c733753e8ec18db4ee7bf259d48
100hoursDEV <98231532+100hoursDEV@users.noreply.github.com>
2022-02-19 19:11:17 -0800

    packages by rank, cool beans

--- first_site/pypi_org/views/package_views.py
+++ first_site/pypi_org/views/package_views.py
@@ -6,6 +6,12 @@ blueprint = flask.Blueprint('packages', __name__, template_folder='templates')
 
 
 @blueprint.route('/project/<package_name>')
-@response(template_file='packages/details.html')
-def package_details(package_name):
+# @response(template_file='packages/details.html')
+def package_details(package_name: str):
     return f"Package details for {package_name}"
+
+
+@blueprint.route('/<int:rank>')
+# @response(template_file='packages/details.html')
+def package_rank(rank: int):
+    return f"Package details for {rank}"
