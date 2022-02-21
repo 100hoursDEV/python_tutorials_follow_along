0c7eddcc1878fdc537f38dc30ae0a97b6fedb880
100hoursDEV <98231532+100hoursDEV@users.noreply.github.com>
2022-02-21 08:04:18 -0800

    Add bare bones HTML to homepage index /

--- data-driven-web-apps-with-flask/follow_along/pypi_org/views/home_views.py
+++ data-driven-web-apps-with-flask/follow_along/pypi_org/views/home_views.py
@@ -5,10 +5,12 @@ import flask
 
 blueprint = flask.Blueprint('home', __name__, template_folder='templates')
 
+
 @blueprint.route('/')
 @response(template_file='home/index.html')
 def index():
     test_packages = package_service.get_latest_packages()
+    print(test_packages)
     return {'packages': test_packages}
 
 
